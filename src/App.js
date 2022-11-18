3994ae2fe288c43f1db6455677399a831b31cb21
hannemsalmi <hanne.salmi@metropolia.fi>
2022-11-18 16:30:04 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const Person = ({ person }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <li>{person.name} {person.number}</li>
+  )
 }
 
-export default App;
+const Persons = (props) => {
+  return(
+  <ul>
+      {props.personstoshow.map(person => <Person key={person.name} person={person} />
+      )}
+      </ul>
+      )
+}
+
+const Filter = (props) => {
+return (
+<div>
+filter shown with<input
+value={props.filter}
+onChange={props.handle} />
+</div>
+)
+}
+
+const PersonForm = (props) => {
+  return(
+<form onSubmit={props.addperson}>
+        <div>
+          name: <input 
+          value={props.newname}
+          onChange={props.namechange}/>
+        <div>number: <input 
+        value={props.newnumber}
+        onChange={props.numberchange}/>
+        </div>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      )
+    }
+
+const App = () => {
+  const [persons, setPersons] = useState([
+  ])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
+
+  const personsToShow = persons.filter(person => person.name.includes(newFilter))
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+
+    if (persons.some(e => e.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+    }
+    else{
+    const personObject = {
+      name: newName,
+      number: newNumber,
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+    setNewNumber('')
+  }
+}
+  
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    console.log(event.target.value)
+    setNewFilter(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter filter={newFilter} handle={handleFilterChange}/>
+      <PersonForm addperson={addPerson} newname={newName} namechange={handleNameChange} 
+      newnumber={newNumber} numberchange={handleNumberChange} />
+      <h2>Numbers</h2>
+      <Persons personstoshow={personsToShow}/>
+    </div>
+  )
+}
+
+export default App
